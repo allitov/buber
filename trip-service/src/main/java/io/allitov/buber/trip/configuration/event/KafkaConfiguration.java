c9371cb81f7a13913303544ca31dd8a7dcb68d54
Alexander Litovkin <litovkinalexan@gmail.com>
2026-05-03 17:14:19 +0700

    feat: add trip creation event

---
+++ trip-service/src/main/java/io/allitov/buber/trip/configuration/event/KafkaConfiguration.java
@@ -0,0 +1,41 @@
+package io.allitov.buber.trip.configuration.event;
+
+import io.allitov.buber.trip.api.event.TripCreatedEvent;
+import java.util.HashMap;
+import java.util.Map;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
+import org.springframework.kafka.support.serializer.JacksonJsonSerializer;
+import tools.jackson.databind.json.JsonMapper;
+
+/**
+ * Конфигурация Kafka.
+ */
+@Configuration
+class KafkaConfiguration {
+
+    @Value("${spring.kafka.bootstrap-servers}")
+    private String bootstrapServers;
+
+    @Bean
+    ProducerFactory<String, TripCreatedEvent> producerFactory(JsonMapper jsonMapper) {
+        Map<String, Object> config = new HashMap<>();
+        config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        config.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        config.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JacksonJsonSerializer.class);
+
+        return new DefaultKafkaProducerFactory<>(
+                config, new StringSerializer(), new JacksonJsonSerializer<>(jsonMapper));
+    }
+
+    @Bean
+    KafkaTemplate<String, TripCreatedEvent> kafkaTemplate(ProducerFactory<String, TripCreatedEvent> producerFactory) {
+        return new KafkaTemplate<>(producerFactory);
+    }
+}
